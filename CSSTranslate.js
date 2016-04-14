0872dd35423aa352500099bd52b6f9a997f83202
Paul Wilson <paul@vermonster.com>
2016-04-14 16:48:58 -0400

    Remove variable that is not defined

--- CSSTranslate.js
+++ CSSTranslate.js
@@ -1,11 +1,5 @@
 module.exports = function (position, axis) {
     var positionCss = (axis === 'horizontal') ? [position, 0] : [0, position];
-
-    if (_has3d) {
-        // adds z position
-        positionCss.push(0);
-    }
-
     var translatedPosition = 'translate(' + positionCss.join(',') + ')';
 
     return translatedPosition;
