f3c985cfc73a7b42a8eb235990e0ff4d41ea899c
Paul Wilson <paul@vermonster.com>
2016-04-18 15:12:31 -0400

    Remove 3d translate causing issues on server

--- src/CSSTranslate.js
+++ src/CSSTranslate.js
@@ -1,16 +1,6 @@
-var has3d = require('./has3d');
-
 module.exports = function (position, axis) {
-    var _has3d = has3d();
     var positionCss = (axis === 'horizontal') ? [position, 0] : [0, position];
-    var transitionProp = _has3d ? 'translate3d' : 'translate';
-    
-    if (_has3d) {
-        // adds z position
-        positionCss.push(0);
-    }
+    var translatedPosition = 'translate(' + positionCss.join(',') + ')';
 
-    var translatedPosition = '(' + positionCss.join(',') + ')';
-
-    return transitionProp + translatedPosition;
+    return translatedPosition;
 };
