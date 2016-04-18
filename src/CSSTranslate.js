3c11ffe9fb74bb78ff8e31a6e7fb42403bc849d8
Paul Wilson <paul@vermonster.com>
2016-04-18 15:22:34 -0400

    move lib to root

--- src/CSSTranslate.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (position, axis) {
-    var positionCss = (axis === 'horizontal') ? [position, 0] : [0, position];
-    var translatedPosition = 'translate(' + positionCss.join(',') + ')';
-
-    return translatedPosition;
-};
